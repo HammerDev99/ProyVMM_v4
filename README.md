17b3981d00fcd1172a00451309843796a4a78cb2
darbela4 <arbe.94@hotmail.com>
2022-10-19 20:04:55 -0500

    update project

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # ProyVMM_v4
 
 ## Proyecto de arreglos de datos (Vectores y Matrices)
+
+Se trata de un proyecto desarrollado en el lenguaje de programación Java que contiene diferentes operaciones con vectores y matrices. La aplicación se ve como lo muestran los siguientes pantallazos:
+
+## Vectores
+
+### Pantalla principal
+
+![alt](src/recursos/PantallaPrincipalVector.JPG)
+
+### Funciones que se pueden ejecutar en el vector generado
+
+![alt](src/recursos/FuncionesVector1.JPG)
+
+![alt](src/recursos/FuncionesVector2.JPG)
+
+## Matrices
+
+### Pantalla pricipal matriz
+
+![alt](src/recursos/PantallaPrincipalMatriz.JPG)
+
+### Funciones que se pueden ejecutar en la matriz generada
+
+![alt](src/recursos/FuncionesMatriz1.JPG)
--- src/proyvmm/Main.java
+++ src/proyvmm/Main.java
@@ -13,10 +13,19 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    /*
     public static void main(String[] args) {
         frmVentanaPrincipal x=new frmVentanaPrincipal();//constructor
         x.setLocationRelativeTo(null);//centra la ventana
         x.setDefaultCloseOperation(frmVentanaPrincipal.EXIT_ON_CLOSE);//para acabar proceso cuando se cierre la ventana desde la x
+        x.setResizable(false);//para que no se pueda modificar el tamaño de la ventana
+        x.setVisible(true);//hace visible la interfaz
+    }*/
+    
+    public static void main(String[] args) {
+        frmContenido x=new frmContenido();//constructor
+        x.setLocationRelativeTo(null);//centra la ventana
+        x.setDefaultCloseOperation(frmContenido.EXIT_ON_CLOSE);//para acabar proceso cuando se cierre la ventana desde la x
         x.setResizable(false);//para que no se pueda modificar el tamaño de la ventana
         x.setVisible(true);//hace visible la interfaz
     }
